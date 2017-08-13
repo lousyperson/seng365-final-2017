fc351fb46d73ba9470f7fabaf075c93c74400aeb
Zhi You Tan <ryan_zhiyou@hotmail.com>
2017-08-13 21:22:58 +1200

    WIP

--- app.js
+++ app.js
@@ -9,8 +9,21 @@ db.connect(function (err) {
         console.log('Unable to connect to MySQL.');
         process.exit(1);
     } else {
+        create_tables();
+
         app.listen(4941, function () {
             console.log('Listening on port: ' + 4941);
         });
     }
 });
+
+function create_tables() {
+    // const query = "create database lab5;" +
+    //     "flush privileges;" +
+    //     "use lab5;";
+    const query = "show databases";
+    db.get().query(query, function (err, rows) {
+        console.log(err);
+        console.log(rows)
+    });
+}
--- config/express.js
+++ config/express.js
@@ -9,5 +9,7 @@ module.exports = function () {
     }));
     app.use(bodyParser.json());
 
+    require('../app/routes/users.server.routes.js')(app);
+
     return app;
 };
