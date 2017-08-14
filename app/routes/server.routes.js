08e7177f2621c91a61774b2dc3bf4164d927e943
Zhi You Tan <ryan_zhiyou@hotmail.com>
2017-08-14 18:10:36 +1200

    added root for testing purpose

--- app/routes/server.routes.js
+++ app/routes/server.routes.js
@@ -2,7 +2,19 @@ const projects = require('../controllers/project.server.controller');
 const rewards = require('../controllers/reward.server.controller');
 const users = require('../controllers/user.server.controller');
 
+// ------TESTING PURPOSE------
+function testing(req, res) {
+    const db = require('../../config/db.js');
+    db.get().query('show tables;', function (err, rows) {
+        res.json(rows);
+    });
+}
+// ---------------------------
+
 module.exports = function (app) {
+    app.route("/")
+        .get(testing);
+
     app.route('/projects')
         .get(projects.list)  // done
         .post(projects.create);  // progress
