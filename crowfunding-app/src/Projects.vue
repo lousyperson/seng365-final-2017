dcc4d7d431a421af4bb9e9bd5341cc7f05a83ea6
Ryan Tan <ryan_zhiyou@hotmail.com>
2017-10-10 23:09:23 +1300

    - updated TODO - added filter for projects - updated individual project page to show title and moved image to below title

--- crowfunding-app/src/Projects.vue
+++ crowfunding-app/src/Projects.vue
@@ -1,42 +1,50 @@
 <template>
     <div>
-        <h1>Projects</h1>
-
         <div v-if="errorFlag" style="color: red;">
             {{ error }}
         </div>
 
         <div v-if="$route.params.projectId">
+            <h1>{{ getSingleProject($route.params.projectId).title }}</h1>
+            <img :src="getImage($route.params.projectId)" height="300" />
+
             <div id="project">
                 <router-link :to="{ name: 'projects' }">Back to Projects</router-link>
 
                 <br /><br />
 
                 <table>
-                    <tr>
-                        <td>Project ID</td>
-                        <td>Title</td>
-                        <td>Subtitle</td>
-                        <td>Image</td>
-                    </tr>
-                    <tr>
-                        <td>{{ $route.params.projectId }}</td>
-                        <td>{{ getSingleProject($route.params.projectId).title }}</td>
-                        <td>{{ getSingleProject($route.params.projectId).subtitle }}</td>
-                        <td><img :src="getImage($route.params.projectId)" height="50%" width="50%" /></td>
-                    </tr>
+                    <thead>
+                        <tr>
+                            <td>Project ID</td>
+                            <td>Subtitle</td>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>{{ $route.params.projectId }}</td>
+                            <td>{{ getSingleProject($route.params.projectId).subtitle }}</td>
+                            <td></td>
+                        </tr>
+                    </tbody>
                 </table>
             </div>
         </div>
 
         <div v-else>
+            <h1>{{ pageTitle }}</h1>
+            <input type="text" id="filterInput" v-on:keyup="filterFunction" placeholder="Filter project..." />
+
             <div id="projects">
                 <table>
                     <!--TODO Check closed projects-->
-                    <tr v-for="project in projects" v-if="project.open === true">
-                        <td>{{ project.title }}</td>
-                        <td><router-link :to="{ name: 'project', params: { projectId: project.id }}">View</router-link></td>
-                    </tr>
+                    <template v-for="project in projects">
+                        <tr v-if="project.open === true" class="projectsRow" :title="getProjectTitle(project.title)">
+                            <td>{{ project.title }}</td>
+                            <td><router-link :to="{ name: 'project', params: { projectId: project.id }}">View</router-link></td>
+                            <td><img :src="getImage(project.id)" height="100" /></td>
+                        </tr>
+                    </template>
                 </table>
             </div>
         </div>
@@ -50,7 +58,8 @@
                 error: "",
                 errorFlag: false,
                 projects: [],
-                projectId: 0
+                projectId: 0,
+                pageTitle: 'Projects'
             }
         },
         mounted: function () {
@@ -77,6 +86,25 @@
 
             getImage: function (id) {
                 return 'http://localhost:4941/api/v2/projects/' + id + '/image'
+            },
+
+            getProjectTitle: function (title) {
+                return title;
+            },
+
+            filterFunction: function () {
+                let input, filter, td;
+                input = document.getElementById('filterInput');
+                filter = input.value.toUpperCase();
+                td = document.querySelectorAll(".projectsRow");
+                td.forEach((value, index, listObj) => {
+                    let title = value.title.toUpperCase();
+                    if (title.indexOf(filter) < 0) {
+                        value.style.display = "none";
+                    } else {
+                        value.style.display = "";
+                    }
+                })
             }
         }
     }
